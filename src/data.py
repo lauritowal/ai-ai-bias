e56d1a1a7188427c30a5865cbd27cffa2ba6e92d
Walter Laurito <Lauritowal@yahoo.com>
2023-11-10 18:59:14 +0100

    remove empty lines before loading

--- src/data.py
+++ src/data.py
@@ -13,12 +13,30 @@ class Entry:
     type: str
     prompt: str
     human_desc: list[str]
+    
+    
+def remove_empty_lines(file):
+    """
+    Remove empty lines from a given file.
 
+    Args:
+    file_path (str): The path to the file from which empty lines should be removed.
+
+    Returns:
+    None: The function modifies the file in-place and does not return anything.
+    """
+
+    lines = file.readlines()
+    no_empty_lines = [line for line in lines if line.strip()]
+    return "".join(no_empty_lines)
 
 def load_data(filename: str):
     print("Loading", filename)
     with open(filename) as f:
-        data = toml.loads(f.read())
+        # needed for TOML to work
+        no_empty_lines = remove_empty_lines(f) 
+        print("no_empty_lines", no_empty_lines)
+        data = toml.loads(no_empty_lines)
         
     return Entry(
         filename=filename,
@@ -36,3 +54,4 @@ def load_all(path):
             continue
         result.append(load_data(os.path.join(path, name)))
     return result
+
