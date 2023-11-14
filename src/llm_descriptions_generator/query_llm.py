eabd7d1c044620bbcf567a45461ebc362556caeb
mochuelo-kun <mochuelokun@gmail.com>
2023-11-14 19:43:06 +0100

    add extra retry functionality

--- src/llm_descriptions_generator/query_llm.py
+++ src/llm_descriptions_generator/query_llm.py
@@ -1,7 +1,16 @@
 import logging
+import time
 
 import langchain
-from interlab.lang_models import  query_model
+from interlab.lang_models import query_model
+from interlab.queries import QueryFailure
+from interlab.queries.experimental.repeat import repeat_on_failure
+from openai.error import (
+    APIError,
+    Timeout,
+    APIConnectionError,
+    RateLimitError,
+)
 
 from llm_descriptions_generator.schema import (
     Engine,
@@ -9,6 +18,9 @@ from llm_descriptions_generator.schema import (
     TextItemGenerationPrompt,
     Origin,
 )
+
+MAX_SUPER_RETRY_COUNT = 10
+SUPER_RETRY_WAIT_INTERVAL_SECONDS = 60
 
 
 def generate_llm_descriptions(
@@ -29,7 +41,30 @@ def generate_llm_descriptions(
     engine = langchain.chat_models.ChatOpenAI(model_name=llm_engine)
 
     for i in range(description_count):
-        desc = query_model(engine, generation_prompt.prompt_text)
+        def query_llm_for_new_description():
+            # TODO: make retry logic more flexible and/or better integrated with interlab/langchain tooling
+            try:
+                return query_model(engine, generation_prompt.prompt_text)
+            except (APIError, Timeout, APIConnectionError) as e:
+                retry_interval = SUPER_RETRY_WAIT_INTERVAL_SECONDS
+                logging.warning(e)
+                logging.warning(f"Retryable error from LLM's API encountered. Will try again after {retry_interval} seconds.")
+                time.sleep(retry_interval)
+                raise QueryFailure # triggers interlab repeat_on_failure behavior
+            except RateLimitError as e:
+                logging.error(e)
+                logging.error("Open AI Rate limit and/or quota exceeded. Shutting down until you wait a while and/or buy new credits.")
+                exit()
+            except Exception as e:
+                raise e
+
+        desc = repeat_on_failure(
+            fn=query_llm_for_new_description,
+            max_repeats=MAX_SUPER_RETRY_COUNT,
+            use_context=False,
+            throw_if_fail=True,
+        )
+        
         descriptions.append(desc)
 
     llm_description_batch = LlmGeneratedTextItemDescriptionBatch(
