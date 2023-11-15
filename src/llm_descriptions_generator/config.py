f077d686ba8ea8e89826a47b8c3b59208bccfd82
mochuelo-kun <mochuelokun@gmail.com>
2023-11-15 13:03:09 +0100

    llm-generated book summaries

--- src/llm_descriptions_generator/config.py
+++ src/llm_descriptions_generator/config.py
@@ -22,7 +22,15 @@ TEXT_GENERATION_PROMPT_CONFIG = {
             prompt_base_text="Read the following reviews of a certain book and write a review of your own for the same book, as if you are a reader who cares about writing quality book reviews. Use a similar level of quality to the given reviews. Try to include only information that was present in the given reviews, and do not invent new information about the book.",
             include_human_descriptions=True,
         )
-    ]
+    ],
+    "book_summary": [
+        TextItemGenerationPromptConfig(
+            item_type="book_summary",
+            prompt_nickname="summary_with_human_version_as_guideline",
+            prompt_base_text="Please write a summary of the following book. A sample summary is included below for reference. Use the sample as a guideline for quality and level of detail.",
+            include_human_descriptions=True,
+        )
+    ],
 }
 
 def get_text_item_generation_prompt_config(
