baf38c9f92e3ac73c4ec0c67638592e9e2284d81
Tomas Gavenciak <gavento@gmail.com>
2024-05-12 21:41:07 +0200

    Improve Groq timing - add exponential backoff to retrying queries

--- src/groq_model.py
+++ src/groq_model.py
@@ -1,6 +1,7 @@
 import functools
 import logging
 import os
+import random
 import time
 import typing as t
 
@@ -9,38 +10,41 @@ import interlab
 
 @functools.lru_cache(maxsize=None)
 class GroqModel(interlab.lang_models.LangModelBase):
-    def __init__(self, model_name: str, min_delay=2.1):
+    def __init__(self, model_name: str, min_delay=1.0, timeout=120.0):
         self.model_name = model_name
         self.min_delay = min_delay
-        self.last_query_time = 0
+        self.timeout = timeout
         import groq
         self.client = groq.Groq(api_key=os.environ.get("GROQ_API_KEY"))
         logging.info(
             f"Instantiated GroqModel {model_name} and key {self.client.api_key[:8]}...{self.client.api_key[-6:]}")
 
     def _query(self, prompt: str, conf: dict[str, t.Any]) -> str:
-        passed = time.time() - self.last_query_time
-        # Note: this may be in the future if we're running multiple queries in parallel
-        # OFC this is a bit of a hack, but it's simple and works well enough
-        if passed < self.min_delay:
-            self.last_query_time = self.last_query_time + self.min_delay
-            logging.info(f"Sleeping for {self.min_delay - passed:.2f} seconds to respect Groq API rate limits.")
-            time.sleep(self.min_delay - passed)
-        else:
-            self.last_query_time = time.time()
+        import groq
+        start = time.time()
+        delay = self.min_delay * random.uniform(0.8, 1.2)
 
-        chat_completion = self.client.with_options(max_retries=0).chat.completions.create(
-            max_tokens=conf["max_tokens"],
-            temperature=conf["temperature"],
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt,
-                }
-            ],
-            model=conf["model_name"],
-        )
-        return chat_completion.choices[0].message.content
+        while True:
+            try:
+                chat_completion = self.client.with_options(max_retries=1).chat.completions.create(
+                    max_tokens=conf["max_tokens"],
+                    temperature=conf["temperature"],
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": prompt,
+                        }
+                    ],
+                    model=conf["model_name"],
+                )
+                return chat_completion.choices[0].message.content
+            except (groq.APIError) as e:
+                if time.time() - start > self.timeout:
+                    raise e
+                logging.info(f"Query failed, retrying after a delay: {e}")
+                time.sleep(delay)
+                delay *= 2
+                
 
     def prepare_conf(self, max_tokens=1024, temperature=1.0) -> tuple[str, dict[str, t.Any]]:
         name = f"query interlab {__class__.__qualname__} ({self.model_name})"
