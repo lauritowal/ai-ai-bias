6638946fabaef86cfef731d343675c37105a1e15
Walter Laurito <lauritowal@yahoo.com>
2024-12-04 23:02:19 +0100

    add from json generated proposal

--- start.sh
+++ start.sh
@@ -4,8 +4,18 @@
 #     --engine='gpt-4-1106-preview' \
 #     --target-count=1
 
-python scripts/generate_llm_descriptions.py \
+# python scripts/generate_llm_descriptions.py \
+#     --item-type=proposal \
+#     --prompt-nickname=from_json_details \
+#     --engine='gpt-4-1106-preview' \
+#     --target-count=1
+
+
+# TODO: NEXT
+python3 scripts/generate_and_compare_descriptions.py \
     --item-type=proposal \
-    --prompt-nickname=from_json_details \
-    --engine='gpt-4-1106-preview' \
-    --target-count=1
+    --comparison-prompt-key=proposal_pick_one \
+    --comparison-engine='gpt-4-1106-preview' \
+    --description-prompt-key=from_json_details \
+    --description-engine='gpt-4-1106-preview' \
+    --min-description-generation-count=4
