d663c461cfefe0ba74057777fdf4aa189ada2bea
Walter Laurito <Lauritowal@yahoo.com>
2025-01-06 20:29:10 +0100

    fix list for comparison models

--- run.paper.sh
+++ run.paper.sh
@@ -2,7 +2,16 @@
 
 WORKERS=1  # If the M starts with `groq-` then set 1 one instead
 
-COMPARISON_MODELS= "together-meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo together-mistralai/Mixtral-8x22B-Instruct-v0.1 together-Qwen/Qwen2.5-72B-Instruct-Turbo gpt-3.5-turbo gpt-4-1106-preview together-Qwen/Qwen2.5-7B-Instruct-Turbo" # together-meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo # ignore 8B for now
+# Note: No space after the '=' and the entire list is wrapped in quotes:
+COMPARISON_MODELS="\
+  together-meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo \
+  together-mistralai/Mixtral-8x22B-Instruct-v0.1 \
+  together-Qwen/Qwen2.5-72B-Instruct-Turbo \
+  gpt-3.5-turbo \
+  gpt-4-1106-preview \
+  together-Qwen/Qwen2.5-7B-Instruct-Turbo\
+"
+
 echo "COMPARISON_MODELS: $COMPARISON_MODELS"
 
 for REPEAT in `seq 1`; do # Repetition to handle errors and crashes, everything is cached so it's fast
