22243f8a6da37845c7daa1551ee2dc54c1186f1e
Walter Laurito <lauritowal@yahoo.com>
2025-01-10 10:34:24 +0100

    add movies

--- run.movies.sh
+++ run.movies.sh
@@ -3,17 +3,15 @@
 
 WORKERS=1  # If the M starts with `groq-` then set 1 one instead
 
-# Note: No space after the '=' and the entire list is wrapped in quotes:
-# COMPARISON_MODELS="\
-#   together-meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo \
-#   together-mistralai/Mixtral-8x22B-Instruct-v0.1 \
-#   together-Qwen/Qwen2.5-72B-Instruct-Turbo \
-#   gpt-3.5-turbo \
-#   gpt-4-1106-preview \
-#   together-Qwen/Qwen2.5-7B-Instruct-Turbo\
-# "
+COMPARISON_MODELS="\
+  together-meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo \
+  together-mistralai/Mixtral-8x22B-Instruct-v0.1 \
+  together-Qwen/Qwen2.5-72B-Instruct-Turbo \
+  gpt-3.5-turbo \
+  gpt-4-1106-preview \
+  together-Qwen/Qwen2.5-7B-Instruct-Turbo\
+"
 
-COMPARISON_MODELS="gpt-4-1106-preview"
 
 echo "COMPARISON_MODELS: $COMPARISON_MODELS"
 
@@ -28,15 +26,15 @@ for REPEAT in `seq 1`; do # Repetition to handle errors and crashes, everything 
             --description-prompt-key=from_title_and_year  \
             --min-description-generation-count=1  \
             --description-engine='gpt-4-1106-preview' \
+            --description-engine='gpt-3.5-turbo' \
+            --description-engine='together-mistralai/Mixtral-8x22B-Instruct-v0.1' \
+            --description-engine='together-Qwen/Qwen2.5-7B-Instruct-Turbo' \
+            --description-engine='together-meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo' \
+            --description-engine='together-meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo' \
             --max-comparison-concurrent-workers="$WORKERS" \
             --redo-invalid-results \
             --min-description-generation-count=1
     done
 done
 
-            # --description-engine='gpt-3.5-turbo' \
-            # --description-engine='together-mistralai/Mixtral-8x22B-Instruct-v0.1' \
-            # --description-engine='together-Qwen/Qwen2.5-7B-Instruct-Turbo' \
-            # --description-engine='together-meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo' \
-            # --description-engine='together-meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo' \
-            # --description-engine='together-meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo' \
+
