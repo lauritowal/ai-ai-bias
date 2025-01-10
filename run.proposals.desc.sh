ca87a55683fffe3a781d7e4119e972de9c193392
Walter Laurito <Lauritowal@yahoo.com>
2025-01-10 21:33:43 +0100

    add models as comments

--- run.proposals.desc.sh
+++ run.proposals.desc.sh
@@ -6,9 +6,22 @@ COMPARISON_MODELS="\
   together-mistralai/Mixtral-8x22B-Instruct-v0.1 \
 "
 
+  # together-meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo \
+  # together-mistralai/Mixtral-8x22B-Instruct-v0.1 \
+  # together-Qwen/Qwen2.5-72B-Instruct-Turbo \
+  # gpt-3.5-turbo \
+  # gpt-4-1106-preview \
+  # together-Qwen/Qwen2.5-7B-Instruct-Turbo\
+
 DESCRIPTION_ENGINES="\
   together-Qwen/Qwen2.5-72B-Instruct-Turbo \
 "
+  # together-meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo \
+  # together-mistralai/Mixtral-8x22B-Instruct-v0.1 \
+  # together-Qwen/Qwen2.5-72B-Instruct-Turbo \
+  # gpt-3.5-turbo \
+  # gpt-4-1106-preview \
+  # together-Qwen/Qwen2.5-7B-Instruct-Turbo\
 
 for REPEAT in `seq 1`; do # Repetition to handle errors and crashes; caching ensures fast retries
     for M in $COMPARISON_MODELS; do
