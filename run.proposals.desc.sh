1853fda66ddeb4ccce3bc64782180b8eb3fd1edf
Walter Laurito <Lauritowal@yahoo.com>
2025-01-11 08:53:20 +0100

    add results

--- run.proposals.desc.sh
+++ run.proposals.desc.sh
@@ -12,18 +12,18 @@ COMPARISON_MODELS="\
 "
 
 DESCRIPTION_ENGINES="\
-  together-Qwen/Qwen2.5-72B-Instruct-Turbo \
+  gpt-3.5-turbo \
 "
 
 # 
-# together-mistralai/Mixtral-8x22B-Instruct-v0.1 \
+# 
 # together-meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo \
-# gpt-3.5-turbo \
 # gpt-4-1106-preview \
 # together-Qwen/Qwen2.5-7B-Instruct-Turbo\
+# together-mistralai/Mixtral-8x22B-Instruct-v0.1 \
 
 # Done description models:
-#  
+# together-Qwen/Qwen2.5-72B-Instruct-Turbo \
 
 
 for REPEAT in `seq 1`; do # Repetition to handle errors and crashes; caching ensures fast retries
